31eb3ba5edb521bb9b617dd6697a0b159910a555
kabeh2 <mattcnk@hotmail.com>
2019-10-02 23:00:56 -0400

    Added useLocalStorage and useDarkMode hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 class App extends Component {
@@ -15,7 +16,7 @@ class App extends Component {
     try {
       const response = await axios.get("http://localhost:5000/api/players");
       const players = response.data;
-      console.log(players);
+      console.log(response);
       this.setState({
         players
       });
@@ -27,6 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App" data-test="component-app">
+        <Navbar />
         <ol data-test="component-players">
           {this.state.players.map(player => (
             <li key={player.id} data-test="component-player">
---
+++ client/src/components/useDarkMode.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = () => {
+  const [darkMode, setValue] = useLocalStorage("hellooo");
+
+  useEffect(() => {
+    if (darkMode) {
+      document.querySelector("body").classList.add("dark-mode");
+    } else {
+      document.querySelector("body").classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+  return [darkMode, setValue];
+};
+
+export default useDarkMode;
